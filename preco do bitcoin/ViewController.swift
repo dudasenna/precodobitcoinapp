01b7d6b96233ef0ef0791f5df5b0e0127315712e
dudasenna <meduardasenna1@gmail.com>
2021-10-01 14:33:30 -0300

    adiciona app preço do bitcoin

--- preco do bitcoin/ViewController.swift
+++ preco do bitcoin/ViewController.swift
@@ -9,15 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var precoBitcoins: UILabel!
+    
+    @IBOutlet weak var botaoAtualizar: UIButton!
+    
+    @IBAction func atualizarPreco(_ sender: Any) {
+        self.recuperarPrecoBitcoin()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.recuperarPrecoBitcoin()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func formatarPreco(preco: NSNumber ) -> String {
+        
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.locale = Locale(identifier: "pt_BR")
+        
+        if let precoFinal = nf.string(from: preco ) {
+            return precoFinal
+        }
+        
+        return "0,00"
+        
+    }
+    
+    func recuperarPrecoBitcoin(){
+        
+        self.botaoAtualizar.setTitle("Atualizando...", for: .normal)
+        if let url = URL(string: "https://blockchain.info/pt/ticker") {
+            let tarefa = URLSession.shared.dataTask(with: url) { (dados, requisicao, erro) in
+                
+                if erro == nil {
+                    
+                    if let dadosRetorno = dados {
+                        
+                        do{
+                            
+                            if let objetoJson = try JSONSerialization.jsonObject(with: dadosRetorno, options: []) as? [String: Any] {
+                                
+                                if let brl = objetoJson["BRL"] as? [String: Any] {
+                                    if let preco = brl["buy"] as? Double {
+                                        let precoFormatado = self.formatarPreco(preco: NSNumber(value: preco ))
+                                        
+                                        DispatchQueue.main.async(execute: {
+                                            self.precoBitcoins.text = "R$ " + precoFormatado
+                                            self.botaoAtualizar.setTitle("Atualizar", for: .normal)
+                                        })
+                                        
+                                        
+                                    }
+                                }
+                                
+                                
+                            }
+                            
+                            
+                        }catch{
+                            print("Erro ao formatar o retorno.")
+                        }
+                        
+                    }
+                    
+                }else{
+                    print("Erro ao fazer a consulta do preço.")
+                }
+                
+            }
+            tarefa.resume()
+            
+        }/*fim IF*/
+        
     }
 
 
